43f8a518fa1156cf2bdd03d85e3d866404f439d4
Subhrajeet <subhrajeet123babu@gmail.com>
2024-08-12 19:42:07 +0530

    First Commit

--- index.js
+++ index.js
@@ -24,7 +24,44 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+// First use the end point "/api/:date?" and use the (req,res) function to get the user input date from req.params.date . 
+// Then, check if a valid date is given in params or not.
+// If (!isNaN(req.params.date)) === True then date is in unix format in string. i.e date = "1451001600000"
+// So, Convert the string into integer using ParseInt.
+// Otherwise execute else block where date is in string format. i.e date = "2015-12-25".
+// If req.params.date === Null .Then, Set date = Current date .
+// After that convert the date to Unix and utc format .
+// if unix == null then return error : Invalid date.
+// At last return the unix, utc in json format.
 
+//My Code Starts Here.
+
+app.get("/api/:date?", function (req,res) {
+  let date;
+  // console.log(req.params.date);
+  if(req.params.date){
+    if(!isNaN(req.params.date)){
+      date = new Date(parseInt(req.params.date))
+    } else { 
+      date = new Date(req.params.date)
+    }
+  } else {
+    date = new Date();
+  }
+
+  const unix = date.getTime();
+  console.log(unix);
+  const utc = date.toUTCString();
+  if (!unix){
+    res.json({error : "Invalid Date"});
+  }
+  res.json({
+    unix: unix,
+    utc: utc
+  })
+  // console.log("unix: "+ unix, "utc: " + utc);
+})
+//My Code Ends Here.
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
